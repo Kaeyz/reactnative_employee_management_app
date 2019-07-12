a3d2800da197ae845302cd0db048b600189f01f8
Agbele Kolade <kolagbele@gmail.com>
2019-07-12 07:32:59 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import Router from "./Router";
 class App extends Component {
   componentWillMount() {
     const config = {
-      apiKey: "AIzaSyC2_rPkLybQtmBS0H22UlabCIPjKDWhSBA",
-      authDomain: "manager-d381d.firebaseapp.com",
-      databaseURL: "https://manager-d381d.firebaseio.com",
-      projectId: "manager-d381d",
-      storageBucket: "manager-d381d.appspot.com",
-      messagingSenderId: "177963173144"
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
     firebase.initializeApp(config);
   }
